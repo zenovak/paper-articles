dc32793b850c32048b85e693534cf990cea2c5f5
Zenovak <xe.novak@outlook.com>
2024-08-14 17:45:58 +0800

    fix: updated styles for github-like inline code

--- pages/examples/2.js
+++ pages/examples/2.js
@@ -10,7 +10,11 @@ export function getStaticProps() {
 export default function WithTailWind({content}){
     return (
         <Markdown
-            className=" max-w-7xl mx-auto prose lg:prose-lg"
+            className={"max-w-7xl mx-auto prose lg:prose-lg " +
+            "prose-inline-code:before:content-none prose-inline-code:after:content-none " +
+            "prose-inline-code:bg-[#f6f6f7] prose-inline-code:px-1.5 prose-inline-code:py-1 " +
+            "prose-inline-code:rounded-md "
+            }
         >
             {content}
         </Markdown>
