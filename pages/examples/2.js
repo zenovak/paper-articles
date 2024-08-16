f1ab74a2aa754eb19c2fda4f701c59193b3e66c8
Zenovak <xe.novak@outlook.com>
2024-08-16 13:09:00 +0800

    refactor: changed example 2 to use vanilla tailwind typography

--- pages/examples/2.js
+++ pages/examples/2.js
@@ -10,10 +10,10 @@ export function getStaticProps() {
 export default function WithTailWind({content}){
     return (
         <Markdown
-            className={"max-w-7xl mx-auto prose lg:prose-lg " +
-            "prose-inline-code:before:content-none prose-inline-code:after:content-none " +
-            "prose-inline-code:bg-[#f6f6f7] prose-inline-code:px-1.5 prose-inline-code:py-1 " +
-            "prose-inline-code:rounded-md "
+            className={"max-w-7xl mx-auto prose lg:prose-lg "
+            // "prose-inline-code:before:content-none prose-inline-code:after:content-none " +
+            // "prose-inline-code:bg-[#f6f6f7] prose-inline-code:px-1.5 prose-inline-code:py-1 " +
+            // "prose-inline-code:rounded-md "
             }
         >
             {content}
