6e17f4188d4cbb065ce71119610f7c04bec9c323
Zenovak <xe.novak@outlook.com>
2024-08-19 21:55:31 +0800

    doc: code wrapper example for react-markdown

---
+++ components/CodeWrapper.js
@@ -0,0 +1,32 @@
+export const CodeWrapper = () => {
+    <div
+        className="code-block"
+    >
+        <div
+            className="code-block-title"
+        >
+            <span
+                className="code-block-title-language"
+            >
+                Label
+            </span>
+                <button 
+                    className="code-block-title-button"
+                onclick="
+                const parent = this.closest('.code-block-root');
+                const codeBlock = parent.querySelector('code')
+                navigator.clipboard.writeText(codeBlock.textContent);">
+                        {/* Icon Here  */}
+                </button>;
+        </div>
+        <div
+            className="code-block-body"
+        >
+            <pre>
+                <code>
+                    need to copy here
+                </code>
+            </pre>
+        </div>
+    </div>
+}
